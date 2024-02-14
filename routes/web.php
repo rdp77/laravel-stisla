15191b9be6e37f34b9d09ec920055bfadf0baf74
Moh Ravi Dwi Putra <ravidwiputra@gmail.com>
2024-02-14 21:13:09 +0700

    :art: delete root route and change default root to /login

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')->name('home');
+Route::redirect('/', '/login')->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
